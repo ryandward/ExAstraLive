0cb08016374f9fa1c8fb38e3da033c154e6c0a83
Ryan David Ward <ryandward@gmail.com>
2025-04-28 23:35:47 -0700

    upped the timeout

--- aegis_readme.sh
+++ aegis_readme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --max-time 10 -k \
+curl --max-time 30 -k \
 	-o rap.html.tmp 'https://p99sanctum.com/index.php/Points' \
 	-H 'Connection: keep-alive' \
 	-H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"' \
